36040d4338501e53002e444dedc4018992c7bf80
Veesh Goldman <rabbiveesh@gmail.com>
2022-07-23 23:10:45 +0300

    get a super simple basic comment parser up

--- grammar.js
+++ grammar.js
@@ -3,43 +3,9 @@ module.exports = grammar({
   rules: {
     source_file: $ => repeat(
       choice(
-        $._statement,
-        $._declaration
+        $.comment
       )
     ),
-    _statement: $ => seq(
-      repeat($._expression),
-      optional($.postfix_modifier)
-      ';'
-    )
-    _declaration: $ => choice(
-      sub_declaration: $ => seq(
-        'sub',
-        $.identifier,
-        optional(
-          choice(
-            seq($.sub_prototype, repeat($.sub_attribute)),
-            seq(repeat($.sub_attribute), $.sub_signature)
-          )
-        ),
-        $.code_block
-      ),
-      phaser_declaration: $ => seq(
-        choice('BEGIN', 'CHECK', 'END', 'INIT', 'UNITCHECK'),
-        $.code_block
-      )
-    ),
-    sub_prototype: $ => seq(
-      '(',
-      repeat(
-        choice(
-          // I think this is correct?
-          /[\\$+@%&*;]/,
-          seq('\[', /[$@%&*]+/, ']')
-        )
-      ),
-      ')'
-    ),
-    code_block: $ => seq('{', repeat($._statement), '}')
+    comment: $ => token(/#.*/)
   }
 })
