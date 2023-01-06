16e41d1a4c94fa71611c65257033ed90ffc41156
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2023-01-06 00:33:47 +0000

    Initial attack at variables

--- grammar.js
+++ grammar.js
@@ -34,9 +34,16 @@ module.exports = grammar({
       $.primitive,
       $.binary_expression,
       $.unary_expression,
+      $._variable,
     ),
     ...primitives,
     binary_expression: $ => choice(...operators.binops($)),
-    unary_expression: $ => choice(...operators.unops($))
+    unary_expression: $ => choice(...operators.unops($)),
+    // TODO: These variables don't handle ${name} or ${^THING} yet
+    _variable: $ => choice($.scalar_var, $.array_var, $.hash_var),
+    scalar_var: $ => seq('$', /\s*/, $._identifier),
+    array_var: $ => seq('@', /\s*/, $._identifier),
+    hash_var: $ => seq('%', /\s*/, $._identifier),
+    _identifier: $ => /[a-zA-Z_]\w*/
   }
 })
