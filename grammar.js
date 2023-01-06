1f04387576ad92e6ae1cdcf3c9ea3dd23f1d99ca
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2023-01-06 10:16:08 +0000

    Reshape grammar.js to more closely follow the shape and structure of perl's own perly.y

--- grammar.js
+++ grammar.js
@@ -1,10 +1,14 @@
 const primitives = require('./lib/primitives.js')
 const operators = require('./lib/operators.js')
 
+/* perl.y defines a `stmtseq` rule, which can match empty. tree-sitter does
+ * not allow this normally, so we'll have to be slightly more complex about it
+ */
+const stmtseq = $ => repeat($._fullstmt);
+
 module.exports = grammar({
   name: 'perl',
   supertypes: $ => [
-    $.statement,
     $.expression,
     $.primitive
   ],
@@ -20,32 +24,64 @@ module.exports = grammar({
     $.comment,
   ],
   rules: {
-    source_file: $ => repeat($.statement),
-    comment: $ => token(/#.*/),
-    statement: $ => choice(
-      $.expression_statement,
-      $.postfix_if_statement,
-      $.postfix_unless_statement,
-      $.postfix_while_statement,
-      $.postfix_until_statement,
-      $.postfix_for_statement,
+    source_file: $ => stmtseq($),
+    /****
+     * Main grammar rules taken from perly.y.
+     ****/
+    block: $ => seq('{', stmtseq($), '}'),
+
+    _fullstmt: $ => choice($._barestmt, /* TODO $._labfullstmt */),
+    // TODO: labfullstmt
+
+    _barestmt: $ => choice(
+      /* TODO: sub */
+      /* TODO: package */
+      /* TODO: use-or-no */
       $.if_statement,
       $.unless_statement,
+      /* TODO: given/when/default */
       $.while_statement,
       $.until_statement,
+      /* TODO: for */
+      seq($.expression_statement, ';'),
+      seq(';'),
     ),
-    expression_statement: $ => seq($.expression, ';'),
-    postfix_if_statement:     $ => seq($.expression, 'if',     field('condition', $.expression), ';'),
-    postfix_unless_statement: $ => seq($.expression, 'unless', field('condition', $.expression), ';'),
-    postfix_while_statement:  $ => seq($.expression, 'while',  field('condition', $.expression), ';'),
-    postfix_until_statement:  $ => seq($.expression, 'until',  field('condition', $.expression), ';'),
-    postfix_for_statement:    $ => seq($.expression, $._for,   field('condition', $.expression), ';'),
-    if_statement:     $ => seq('if',     '(', field('condition', $.expression), ')', field('body', $._block)),
-    // TODO: handle elsif + else
-    unless_statement: $ => seq('unless', '(', field('condition', $.expression), ')', field('body', $._block)),
-    while_statement:  $ => seq('while',  '(', field('condition', $.expression), ')', field('body', $._block)),
-    until_statement:  $ => seq('until',  '(', field('condition', $.expression), ')', field('body', $._block)),
-    _block: $ => seq('{', repeat($.statement), '}'),
+    if_statement: $ =>
+      seq('if', '(', field('condition', $.expression), ')',
+        field('block', $.block),
+      ),
+    unless_statement: $ =>
+      seq('unless', '(', field('condition', $.expression), ')',
+        field('block', $.block),
+      ),
+    while_statement: $ =>
+      seq('while', '(', field('condition', $.expression), ')',
+        field('block', $.block),
+      ),
+    until_statement: $ =>
+      seq('until', '(', field('condition', $.expression), ')',
+        field('block', $.block),
+      ),
+
+    // perly.y calls this `sideff`
+    expression_statement: $ => choice(
+      $.expression,
+      $.postfix_if_expression,
+      $.postfix_unless_expression,
+      $.postfix_while_expression,
+      $.postfix_until_expression,
+      $.postfix_for_expression,
+    ),
+    postfix_if_expression:     $ => seq($.expression, 'if',     field('condition', $.expression)),
+    postfix_unless_expression: $ => seq($.expression, 'unless', field('condition', $.expression)),
+    postfix_while_expression:  $ => seq($.expression, 'while',  field('condition', $.expression)),
+    postfix_until_expression:  $ => seq($.expression, 'until',  field('condition', $.expression)),
+    postfix_for_expression:    $ => seq($.expression, $._for,   field('list', $.expression)),
+
+    /****
+     * Misc bits
+     */
+    comment: $ => token(/#.*/),
     expression: $ => choice(
       $.primitive,
       $.binary_expression,
