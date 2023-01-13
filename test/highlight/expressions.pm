045420c47bca242e41b6dcd5a9edc1a5b98e872c
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2023-01-13 14:29:39 +0000

    Add a queries/highlights.scm plus some unit tests for it

---
+++ test/highlight/expressions.pm
@@ -0,0 +1,16 @@
+do { 123; };
+# <- keyword
+#    ^ number
+require 123;
+# <- keyword
+#       ^ number
+require;
+# <- keyword
+next;
+# <- keyword
+last LOOP;
+# <- keyword
+redo;
+# <- keyword
+goto LABEL;
+# <- keyword
---
+++ test/highlight/statements.pm
@@ -0,0 +1,73 @@
+123 if 45;
+#   ^ keyword
+123 unless 45;
+#   ^ keyword
+123 while 45;
+#   ^ keyword
+123 until 45;
+#   ^ keyword
+123 for 45;
+#   ^ keyword
+123 foreach 45;
+#   ^ keyword
+if(1) { 123; } elsif(2) { 456; } else { 789; }
+# <- keyword
+#              ^ keyword
+#                                ^ keyword
+unless(1) { 123; }
+# <- keyword
+while(1) { 123; }
+# <- keyword
+until(1) { 123; }
+# <- keyword
+for (1, 2, 3) { 456; }
+# <- keyword
+foreach (1, 2, 3) { 456; }
+# <- keyword
+for $V (1, 2, 3) { 456; }
+# <- keyword
+#   ^ variable
+foreach $V (1, 2, 3) { 456; }
+# <- keyword
+#       ^ variable
+for my $x (1, 2, 3) { 456; }
+# <- keyword
+#   ^ keyword
+#      ^ variable
+foreach my $x (1, 2, 3) { 456; }
+# <- keyword
+#       ^ keyword
+#          ^ variable
+for (my $i = 0; $i < 10; $i++) { 123; }
+# <- keyword
+#    ^ keyword
+#       ^ variable
+#               ^ variable
+#                        ^ variable
+use 5.014;
+# <- keyword
+#   ^ number
+use v5.14;
+# <- keyword
+#   ^ number
+use strict;
+# <- keyword
+#   ^ type
+use List::Util 1.23;
+# <- keyword
+#   ^^^^^^^^^^ type
+#              ^ number
+package AAA;
+# <- keyword
+#       ^ type
+package BBB 1.23;
+# <- keyword
+#       ^ type
+#           ^ number
+package CCC { }
+# <- keyword
+#       ^ type
+package DDD 4.56 { }
+# <- keyword
+#       ^ type
+#           ^ number
