da4f61ee209f62dcb7d516a4bf94c989d2d4ac4e
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2023-06-12 14:33:17 +0100

    Also recognise m// regexps

---
+++ test/highlight/regexp.pm
@@ -0,0 +1,23 @@
+qr/pattern/;
+# <- string.regexp
+qr/pattern with (parens)/;
+# <- string.regexp
+qr/pattern with $interpolation/;
+# <- string.regexp
+#               ^ variable.scalar
+qr'pattern with no $interpolation';
+# <- string.regexp
+qr/pattern/i;
+# ^^^^^^^^^^ string.regexp
+
+m/pattern/;
+# <- string.regexp
+m/pattern with (parens)/;
+# <- string.regexp
+m/pattern with $interpolation/;
+# <- string.regexp
+#               ^ variable.scalar
+m'pattern with no $interpolation';
+# <- string.regexp
+m/pattern/i;
+# ^^^^^^^^^^ string.regexp
