09e3a74ca95ba76eac7efddb9bfbe90bf8a8c723
Veesh Goldman <rabbiveesh@gmail.com>
2023-10-11 18:51:06 +0000

    feat(injections): add perl injection for s///e!!

--- queries/injections.scm
+++ queries/injections.scm
@@ -1,2 +1,15 @@
 ; an injections.scm file for nvim-treesitter
-(pod) @pod
+((comment) @injection.content
+ (#set! injection.language "comment"))
+ 
+((pod) @injection.content
+ (#set! injection.language "pod"))
+
+(
+  (substitution_regexp
+    (replacement) @injection.content
+    ; match if there's a single `e` in the modifiers list
+    (substitution_regexp_modifiers) @modifiers (#match? @modifiers "e")
+  )
+  (#set! injection.language "perl")
+)
