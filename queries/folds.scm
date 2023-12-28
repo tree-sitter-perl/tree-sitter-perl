4bfaa28b215d5df6a48db37617bf237d794fd925
GH Actions [beep-boop] <release-flow@beep.boop>
2023-12-28 15:07:40 +0000

    deploy: ba251f02ab67833aec0bde679eb701864597717c

--- queries/folds.scm
+++ queries/folds.scm
@@ -3,14 +3,19 @@
 
 ; fold the block-typed package statements only
 (package_statement (block)) @fold
+(class_statement (block)) @fold
 
 [(subroutine_declaration_statement)
+ (method_declaration_statement)
  (conditional_statement)
  (loop_statement)
  (for_statement)
  (cstyle_for_statement)
  (block_statement)
+ (defer_statement)
  (phaser_statement)] @fold
+
+(try_statement (block) @fold)
 
 (anonymous_subroutine_expression) @fold
 
