2ddef25516842adbebe9bfec12888fe2f6397899
GH Actions [beep-boop] <release-flow@beep.boop>
2026-06-14 13:37:30 +0000

    deploy: f1c96741ebbca65a4750fddc43cb3b5fb101c94f

--- queries/injections.scm
+++ queries/injections.scm
@@ -12,3 +12,8 @@
   (#match? @_modifiers "e")
   (#not-match? @_modifiers "e.*e")
   (#set! injection.language "perl"))
+
+; inject a language into a heredoc body based on its terminator, so
+; `my $q = <<SQL; ... SQL` highlights the body as SQL, `<<HTML` as HTML, etc.
+((heredoc_content
+  (heredoc_end) @injection.language) @injection.content)
